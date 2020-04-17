b702b97fcb3e23e806063768b1ca6c9c2b3651cd
David Galvis <ivan.galvis@csgi.com>
2020-04-17 10:21:05 -0500

    Pagination buttons added

--- src/js/index.js
+++ src/js/index.js
@@ -52,3 +52,19 @@ elements.searchForm.addEventListener('submit', e => {
 // const search = new Search('pizza');
 // console.log(search);
 // search.getResults();
+
+//In the controller we should have all our event listeners and we can control what should happen from there
+
+elements.searchResPages.addEventListener('click', e=> {
+    //With this function, we make something of event delegation, in order to know where the click happened, based on that what we want to achieve is to get the data property of the .btn-inline object, but we are receiving clicks from objects inside that one, hence, with the closest method what we want to achieve is to get the closest parent ancestor, no matter when we make the click in the UI
+    const btn = e.target.closest('.btn-inline')
+    if(btn){
+        //Through the html defined, we can set a property data-goto, and manupulate it depending on our needs
+        const goToPage = parseInt(btn.dataset.goto, 10);
+        //We clear the results in order to avoid some conflict
+        searchView.clearResults();
+        //Here we call the function defined in the view, where we can send the array of results and the page we want it to go
+        searchView.renderResults(state.search.result,goToPage);
+        console.log(goToPage);
+    }   
+});
--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -12,7 +12,10 @@ export const clearInput = () =>{
 };
 
 export const clearResults = () => {
+    //This is the way we clear out the inner list of the recipes displayed
     elements.searchResList.innerHTML = '';
+    //Here we clear out the buttons, so that we can reinsert them from the controller
+    elements.searchResPages.innerHTML= '';
 };
 
 //'Pasta with tomato and spinach'
@@ -47,12 +50,48 @@ const renderRecipe = recipe => {
                             <p class="results__author">${recipe.publisher}</p>
                         </div>
                     </a>
-                </li>`
+                </li>
+                `;
     //Inserting this html element ensures that we can get our recipe html linked with the original UI view, the before end injection is the best in this case
     elements.searchResList.insertAdjacentHTML('beforeend',markup);
-}
+};
 
-export const renderResults = recipes =>{
-    //Here we print the whole array that brings the recipes for some of the queries in the controller, in the UI. To do so, we call another function that renders each of the recipes. We don't nwwd to call a arrow function inside te foreach due that we have already done it in the  
-    recipes.forEach(renderRecipe);
+const createButton = (page,type) =>`
+                <button class="btn-inline results__btn--${type}" data-goto="${type === 'prev' ? page - 1 : page + 1}">
+                    <svg class="search__icon">
+                        <use href="img/icons.svg#icon-triangle-${type === 'prev'?'left':'right'}"></use>
+                    </svg>
+                    <span>Page ${type === 'prev' ? page-1 : page+1}</span>
+                </button>
+                `;
+
+const renderButtons = (page, numResults,resPerPage) =>{
+    const pages = Math.ceil(numResults/resPerPage);
+    let button;
+    if(page === 1 && pages>1){
+        //Only the button to the next page
+        button = createButton(page,'next');
+    }else if(page>1 && page<pages){
+        //Here will be the both buttons
+        button = `
+                ${createButton(page, 'next')}
+                ${createButton(page, 'prev')}
+                `;
+    }else if(page === pages & pages >1){
+        //The previous page button
+        button = createButton(page, 'prev');
+    }
+    elements.searchResPages.insertAdjacentHTML('afterbegin',button);
+};
+
+export const renderResults = (recipes, page=1, resPerPage = 10) =>{
+    //Here we print the whole array that brings the recipes for some of the queries in the controller, in the UI. To do so, we call another function that renders each of the recipes. We don't nwwd to call a arrow function inside te foreach due that we have already done it in the
+
+    const start = (page-1)*resPerPage;
+    //In the slice function it's not included the end, this is why there's 
+    const end = page*resPerPage;
+    //With the slice method we take a portion of an array and select that into a variable
+    recipes.slice(start,end).forEach(renderRecipe);
+
+    renderButtons(page, recipes.length,resPerPage);
 }
